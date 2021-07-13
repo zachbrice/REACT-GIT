59be782b21aab30565ec6d7d0107247d00655efb
zachbrice <zbrice@banyanlabs.io>
2021-07-13 11:12:49 -0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          zachy reacty
         </a>
       </header>
     </div>
